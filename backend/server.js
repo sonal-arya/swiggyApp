9225ef6a02268fdfe172fc45551e4afb9965bcd1
Sonal arya <sonalarya1430@gmail.com>
2024-11-14 23:44:38 +0530

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const authRoutes = require('./routes/auth')
 const dataRoutes = require('./routes/data')
 const cors = require('cors')
 const corsOptions = {
-    origin: 'http://localhost:1234', 
+    origin: 'https://swiggypro.netlify.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
     optionsSuccessStatus: 204
